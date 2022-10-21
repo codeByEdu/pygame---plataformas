b19ab858daf8f4ad8f07614558225f499afb08b5
edupn <71460754+edupn@users.noreply.github.com>
2022-10-21 18:10:17 -0300

    adicionado orinetação a objeto, erro o modulo BALA

---
+++ Personagem/Zumbi.py
@@ -0,0 +1,9 @@
+import pygame
+class Zumbi(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load("Sprites/zumbi.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
+
---
+++ index.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+import random
+from Personagem.Personagem import Personagem 
+from Personagem.Zumbi import Zumbi
+from Personagem.Bala import Bala
+
+clock = pygame.time.Clock()
+largura = 890
+altura = 750
+
+pygame.init()
+imagemFundo = pygame.image.load('Sprites/bg.jpg')
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("RPG do SI")
+all_sprites = pygame.sprite.Group()
+jogador = Personagem()
+
+jogando = True
+grupo_de_alvos = pygame.sprite.Group()
+
+for i in range(5):
+    zumbi = Zumbi(random.randrange(0, largura), random.randrange(0, altura))
+    grupo_de_alvos.add(zumbi)
+
+while jogando:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+   
+    all_sprites.add(jogador)    
+    all_sprites.update()
+
+
+
+    tela.blit(imagemFundo, (0, 0))
+    grupo_de_alvos.draw(tela)
+    jogador.colocar(tela)
+    pygame.display.update()
+
+    pygame.display.flip()
+    clock.tick(60)
