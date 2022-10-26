d4247d6a9aeda8821a74ef7b5b60c178ef712974
edupn <71460754+edupn@users.noreply.github.com>
2022-10-26 02:40:05 -0300

    Adicionado paralaxe e tiros na direção que anda

--- Personagem/Bala.py
+++ Personagem/Bala.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 class Bala(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, direcao):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10, 20))
         self.image.fill((0, 255, 0))
@@ -10,18 +10,12 @@ class Bala(pygame.sprite.Sprite):
         self.rect.centerx = x
         self.speedx = -10
         self.speedy = -10
+        self.direcao = direcao
     def update(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-    
-        distance_x = mouse_x - self.rect.centerx
-        distance_y = mouse_y - self.rect.bottom
-    
-        angle = math.atan2(distance_y, distance_x)
-    
-        speed_x = 20 * math.cos(angle)
-        speed_y = 20 * math.sin(angle)
-        self.rect.x += speed_x
-        self.rect.y += speed_y
+        if self.direcao == 'esquerda':
+            self.rect.x += self.speedx
+        else:
+            self.rect.x -= self.speedx
         # kill if it moves off the top of the screen
         if self.rect.bottom < 0:
             self.kill()
--- index.py
+++ index.py
@@ -6,12 +6,15 @@ from Personagem.Zumbi import Zumbi
 from Personagem.Bala import Bala
 
 clock = pygame.time.Clock()
-largura = 890
-altura = 750
+largura = 800
+altura = 432
 
 pygame.init()
-imagemFundo = pygame.image.load('Sprites/bg.jpg')
 tela = pygame.display.set_mode((largura, altura))
+
+
+
+#imagemFundo = pygame.image.load('Sprites/bg.jpg')
 pygame.display.set_caption("RPG do SI")
 all_sprites = pygame.sprite.Group()
 jogador = Personagem()
@@ -21,11 +24,33 @@ jogando = True
 grupo_de_alvos = pygame.sprite.Group()
 
 for i in range(5):
-    zumbi = Zumbi(random.randrange(0, largura), random.randrange(0, altura))
+    zumbi = Zumbi(random.randrange(0, largura), 350)
     grupo_de_alvos.add(zumbi)
+scroll = 0 
+ground_image = pygame.image.load('Sprites/ground.png').convert_alpha()
+ground_width = ground_image.get_width()
+ground_height = ground_image.get_height()
+
+bg_images = []
+for i in range(1, 6):
+  bg_image = pygame.image.load(f'Sprites/plx-{i}.png').convert_alpha()
+  bg_images.append(bg_image)
+bg_width = bg_images[0].get_width()
+
+def draw_bg():
+  for x in range(5):
+    speed = 1
+    for i in bg_images:
+      tela.blit(i, ((x * bg_width) - scroll * speed, 0))
+      speed += 0.2
+
+def draw_ground():
+  for x in range(15):
+    tela.blit(ground_image, ((x * ground_width) - scroll * 2.5, altura - ground_height))
 
 while jogando:
-
+    draw_bg()
+    draw_ground()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -34,10 +59,17 @@ while jogando:
             if event.key == pygame.K_SPACE:
                 
                 jogador.shoot(all_sprites,tiros)
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] and scroll > 0:
+        scroll -= 3
+        all_sprites.update()
+    if key[pygame.K_d] and scroll < 3000:
+        scroll += 3
+        all_sprites.update()
 
     all_sprites.update()
-
-    tela.blit(imagemFundo, (0, 0))
+    
+    tela.blit(ground_image, (0, 0))
     grupo_de_alvos.draw(tela)
     jogador.colocar(tela)
     pygame.display.update()
